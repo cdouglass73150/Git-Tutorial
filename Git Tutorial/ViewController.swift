fa133e05cab0d5b1e1e75f32e413e79d31d87414
Cary Douglass <cdouglass1950@icloud.com>
2018-04-05 09:09:34 -0700

    Initial Commit

---
+++ Git Tutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Git Tutorial
+//
+//  Created by Cary Douglass on 4/5/18.
+//  Copyright © 2018 Cary Douglass. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
