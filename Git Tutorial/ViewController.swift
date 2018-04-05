e82b693921c20f703836422f33e00994b3f9749c
Cary Douglass <cdouglass1950@icloud.com>
2018-04-05 10:05:48 -0700

    added label and outlet

--- Git Tutorial/ViewController.swift
+++ Git Tutorial/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     
     let message = "hello"
+    @IBOutlet weak var messageLabel:UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
