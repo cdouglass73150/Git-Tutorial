44ce360035766b3d0eb90419ad8758b8e4eb7a0f
Cary Douglass <cdouglass1950@icloud.com>
2018-04-05 09:25:01 -0700

    added a print message

--- Git Tutorial/ViewController.swift
+++ Git Tutorial/ViewController.swift
@@ -9,10 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let message = "hello"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print(message)
     }
 
     override func didReceiveMemoryWarning() {
